99c03eb8bfa6843615b44d8e69fe6698e328d753
XT <e@xtendo.org>
2017-01-28 19:56:13 +0900

    More fish-like distribution

--- init.fish
+++ init.fish
@@ -3,6 +3,20 @@ if not [ -d ~/.pyenv ]
     git clone --depth=1 https://github.com/yyuu/pyenv ~/.pyenv
     git clone --depth=1 https://github.com/yyuu/pyenv-virtualenv ~/.pyenv/plugins/pyenv-virtualenv
 end
-set -x PATH $HOME"/.pyenv/bin" $PATH
-. (pyenv init -|psub)
-. (pyenv virtualenv-init -|psub)
+setenv PATH $HOME"/.pyenv/bin" $PATH
+setenv PATH $HOME'/.pyenv/shims' $PATH
+setenv PYENV_SHELL fish
+. $HOME'/.pyenv/libexec/../completions/pyenv.fish'
+command pyenv rehash 2>/dev/null
+
+setenv PATH $HOME'/.pyenv/plugins/pyenv-virtualenv/shims' $PATH;
+setenv PYENV_VIRTUALENV_INIT 1;
+function _pyenv_virtualenv_hook --on-event fish_prompt;
+  set -l ret $status
+  if [ -n "$VIRTUAL_ENV" ]
+    pyenv activate --quiet; or pyenv deactivate --quiet; or true
+  else
+    pyenv activate --quiet; or true
+  end
+  return $ret
+end
