7f44b4cc41fe461b050afa02ee64602ddf373168
XT <e@xtendo.org>
2017-07-11 12:52:20 +0900

    setenv → set

--- init.fish
+++ init.fish
@@ -3,14 +3,14 @@ if not [ -d ~/.pyenv ]
     git clone --depth=1 https://github.com/yyuu/pyenv ~/.pyenv
     git clone --depth=1 https://github.com/yyuu/pyenv-virtualenv ~/.pyenv/plugins/pyenv-virtualenv
 end
-setenv PATH $HOME"/.pyenv/bin" $PATH
-setenv PATH $HOME'/.pyenv/shims' $PATH
-setenv PYENV_SHELL fish
+set PATH $HOME"/.pyenv/bin" $PATH
+set PATH $HOME'/.pyenv/shims' $PATH
+set PYENV_SHELL fish
 . $HOME'/.pyenv/libexec/../completions/pyenv.fish'
 command pyenv rehash 2>/dev/null
 
-setenv PATH $HOME'/.pyenv/plugins/pyenv-virtualenv/shims' $PATH;
-setenv PYENV_VIRTUALENV_INIT 1;
+set PATH $HOME'/.pyenv/plugins/pyenv-virtualenv/shims' $PATH;
+set PYENV_VIRTUALENV_INIT 1;
 function _pyenv_virtualenv_hook --on-event fish_prompt;
   set -l ret $status
   if [ -n "$VIRTUAL_ENV" ]
